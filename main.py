210c62777a0b9c107c3e1795fc9120fdb9606c3d
Brighton Zhang <brighton.zhang23@ycdsbk12.ca>
2021-01-19 15:59:11 +0000

    finished intro

--- main.py
+++ main.py
@@ -1,51 +1,47 @@
-""" 
-A basic pygame template
-"""
- 
-import pygame
- 
-# Define some colors
-BLACK    = (   0,   0,   0)
-WHITE    = ( 255, 255, 255)
-GREEN    = (   0, 255,   0)
-RED      = ( 255,   0,   0)
- 
-pygame.init()
-  
-# Set the width and height of the screen [width, height]
-size = (700, 500)
-screen = pygame.display.set_mode(size)
- 
-pygame.display.set_caption("My Game")
- 
-#Loop until the user clicks the close button.
-done = False
- 
-# Used to manage how fast the screen updates
-clock = pygame.time.Clock()
- 
-# -------- Main Program Loop -----------
-while not done:
-    # --- Main event loop
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            done = True # Flag that we are done so we exit this loop
-  
-    # --- Game logic should go here
- 
-    # --- Drawing code should go here
-     
-    # First, clear the screen to white or whatever background colour. 
-    # Don't put other drawing commands above this, or they will be erased with this command.
-    screen.fill(WHITE)
-     
-    # --- Go ahead and update the screen with what we've drawn.
-    pygame.display.flip()
- 
-    # --- Limit to 60 frames per second
-    clock.tick(60)
-     
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-pygame.quit()
+import time
+
+#play again
+def playagain():
+    answer = input("Would you like to play/play again? Y or N: ")
+    if answer == "Y":
+        time.sleep(1.5)
+        print("Ok we will begin shortly")
+        time.sleep(4)
+        introduction()
+
+    elif answer == "N":
+        print("See you next time!")
+
+#intro/part 1
+def introduction():
+    print("You are in deep slumber.")
+    time.sleep(3)
+    print("With the distinct voices in the background, you wake up to the morning sun.")
+    time.sleep(1.5)
+    time.sleep(3)
+    print("You feel groggy. Trying to go back to sleep, you are suddenly met with a eerie creature laying under your bed.")
+    time.sleep(1.5)
+    print("Looks like the creature came from the ancient underground caves called the void. The creature is purple and small.")
+    option_1 = input(" You can kick it or run outside. Kick or Run?: ")
+    if option_1 == "Kick":
+        print("You kick it with all of your might. The creature lets out a terrifying shriek, but slowly shrivels away. You Run outside.")
+    
+    elif option_1 == "Run":
+        print("You run outside, only to find ominous creatures roaming around. You look off into the distance. A huge hole has seemed to open. What is it? You head over the hole to peer over the edge")
+    
+    else: 
+        print("Type Kick or Run.")
+        option_1 = input(" You can kick it or run outside. Kick or Run?: ")
+        
+            
+
+
+playagain()
+
+
+#name
+name = input("You suddenly realize that you had forgotten your name, so you decide to name yourself. State your name:")
+
+while name == (""):
+    print("State an actual name.")
+    name = input("You suddenly realize that you had forgotten your name, so you decide to name yourself. State your name:")
